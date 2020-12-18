83fa5e20f1b1da7b5aaea888e74218e6abcc1d80
Artyom <75999395+yuvelir2@users.noreply.github.com>
2020-12-18 23:35:40 +0300

    Add files via upload

---
+++ 12.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <uuid/uuid.h>
+#include <grp.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Process id: %d\n", getpid());
+    printf("Process parent id: %d\n", getppid());
+    struct passwd *pwd;
+    if (!(pwd = getpwuid(getuid())))
+    {
+        perror("Error");
+        return 0;
+    }
+    printf("user id: %d\n", pwd->pw_uid);
+    printf("user name: %s\n", pwd->pw_name);
+    printf("group id: %d\n", pwd->pw_gid);
+    struct group *grp;
+    if (!(grp = getgrgid(pwd->pw_gid)))
+    {
+        perror("Error");
+        return 0;
+    }
+    printf("group name: %s\n", grp->gr_name);
+    return 0;
+}
