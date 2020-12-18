dee6a6e6457793cd459d67fa686db7666561651c
Artyom <75999395+yuvelir2@users.noreply.github.com>
2020-12-18 17:20:54 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#ifdef S_BLKSIZE
+#define BL_SIZE S_BLKSIZE
+#else
+#include <sys/param.h>
+#define BL_SIZE DEV_BSIZE
+#endif
 
-int main(int argc, char *argv[])
+const char *mode_to_str(mode_t file_mode)
 {
-    struct stat stat_buffer;
-    if (argc != 2)
-    {
-        printf("problems with arguments");
-        return 42;
-    }
-
-    if (lstat(argv[1], &stat_buffer) == -1)
-    {
-        perror("lstat didn't work out");
-        return 43;
-    }
-    printf("file type ");
     switch (stat_buffer.st_mode & S_IFMT)
     {
     case S_IFBLK:  printf("block device\n");break;
@@ -30,11 +22,26 @@ int main(int argc, char *argv[])
     case S_IFSOCK: printf("socket\n");break;
     default: printf("unknown?\n");break;
     }
+}
 
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: %s some.file\n", argv[0]);
+        return 42;
+    }
+    struct stat stat_buffer;
+    if (lstat(argv[1], &stat_buffer) == -1)
+    {
+        perror("failed to lstat");
+        return 43;
+    }
+    printf("file type %s\n", mode_to_str(stat_buffer.st_mode));
     printf("mode: %06o\n", stat_buffer.st_mode);
     printf("size: %llu\n", (unsigned long long)stat_buffer.st_size);
-    printf("space used: %llu\n", (unsigned long long)stat_buffer.st_blocks * 512);
-    printf("last file commit:   %s", ctime(&stat_buffer.st_mtime));
-
+    printf("space used: %llu\n", (unsigned long long)stat_buffer.st_blocks * BL_SIZE);
+    printf("last file modification UTC+03:00:   %s", ctime(&stat_buffer.st_mtime));
     return 0;
 }
