daaff24820cc35bb091732d344e5283aa956216b
Artyom <75999395+yuvelir2@users.noreply.github.com>
2020-12-18 16:33:57 +0300

    Update 9.c

--- 9.c
+++ 9.c
@@ -4,16 +4,14 @@
 int main(int argc, char* argv[])
 {
     if (argc != 2)
-        {
-            printf ("epic fail bro\n");
-            return 42;
-        }
-
+    {
+        printf("Usage: %s some.file\n", argv[0]);
+        return 42;
+    }
     struct statfs buffer;
-
     if ((statfs(argv[1], &buffer)) == -1)
     {
-        perror("statfs didn't work out");
+        perror("failed to statfs");
         return 42;
     }
     printf("total: %lu \n",buffer.f_blocks * buffer.f_bsize);
