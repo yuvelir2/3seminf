9e3ee71fb67d6f8bf28e9ce797c12a39288706ba
Artyom <75999395+yuvelir2@users.noreply.github.com>
2020-12-21 16:00:46 +0300

    Update 6-1.c

--- 6-1.c
+++ 6-1.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 {
     if (argc > 2)
     {
-        printf("Usage: %s [dir]", argv[1]);
+        printf("Usage: %s [dir]", argv[0]);
         return 42;
     }
     const char *dirp = (argc == 2) ? argv[1] : ".";
