45c2843a1559fb8c936cca67f6e1f60049058173
Artyom <75999395+yuvelir2@users.noreply.github.com>
2020-12-21 15:52:21 +0300

    Update 9.c

--- 9.c
+++ 9.c
@@ -15,8 +15,9 @@ int main(int argc, char* argv[])
         perror("failed to statvfs");
         return 42;
     }
-    printf("total: %lu \n",buffer.f_blocks * buffer.f_bsize);
-    printf("avaliable: %lu\n", buffer.f_bavail * buffer.f_bsize);
-    printf("used: %lu\n",buffer.f_blocks * buffer.f_bsize - buffer.f_bavail * buffer.f_bsize);
+    printf("total: %lu \n",buffer.f_blocks * buffer.f_frsize);
+    printf("available: %lu\n", buffer.f_bavail * buffer.f_frsize);
+    printf("free: %lu\n", buffer.f_bfree * buffer.f_frsize);
+    printf("used: %lu\n",buffer.f_blocks * buffer.f_frsize - buffer.f_bavail * buffer.f_frsize);
     return 0;
 }
